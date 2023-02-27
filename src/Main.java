b9a59c6f1934a7f2e2365e80f2440bb8b6f1dfaa
Guilherme Marques Rizzatto Teixeira <guimarquesrz@gmail.com>
2023-02-27 00:36:23 -0300

    Mortgage calculator simplified the code

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+       final int months = 12;
+
+       //Inputs and saving the values
        Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float porcentage = (annualInterest /100)/12;
-        System.out.print("Period (Years): ");
-        byte period = scanner.nextByte();
-        int paymentsPerYears = period*12;
-        double count = porcentage * (Math.pow((1+porcentage),paymentsPerYears));
-        double count2 = Math.pow((1+porcentage),paymentsPerYears)-1;
-        double division = count/count2;
-        double multiplication = principal * division;
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        System.out.println("Mortgage: " + currency.format(multiplication));
+       System.out.print("Principal: ");
+       int principal = scanner.nextInt();
+       System.out.print("Annual Interest Rate: ");
+       float annualInterest = scanner.nextFloat();
+       System.out.print("Period (Years): ");
+       byte period = scanner.nextByte();
+
+       //calculation for porcentage of annual interest rate divided by months
+       float porcentage = (annualInterest /100)/months;
+
+       //calculation for know how many payments do you have for years
+       int paymentsPerYears = period*months;
+
+       //calculation  mortgage
+       double count = principal
+              * (porcentage * (Math.pow(1+porcentage,paymentsPerYears)))
+              / (Math.pow(1+porcentage,paymentsPerYears)-1);
+
+       //formating the calculation values for your country
+       String currency = NumberFormat.getCurrencyInstance().format(count);
+
+       //result
+       System.out.println("Mortgage: " + currency);
     }
 }
